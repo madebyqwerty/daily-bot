9f03206ca930d625c618e07a3502625d5db57088
Venca321 <login@vaclavparma.cz>
2023-04-23 20:21:56 +0200

    vytvareni a uzavirani vlaken

--- main.py
+++ main.py
@@ -24,6 +24,12 @@ async def timer():
         current_time = now.strftime("%H:%M:%S")
         if current_time == REPORT_TIME and (not now.weekday() == 4 or not now.weekday() == 5):
             channel = client.get_channel(CHANNEL_ID)
-            await channel.send(MESSAGE)
+            message = await channel.send(MESSAGE.replace("%date%", datetime.datetime.now().strftime("%d/%m")))
+            thread_channel = await channel.create_thread(name=datetime.datetime.now().strftime("%d/%m"), message=message)
+            await close_thread(thread_channel)
+
+async def close_thread(thread_channel):
+    await asyncio.sleep(60*(24-int(REPORT_TIME.split(":")[0])))
+    await thread_channel.edit(archived=True)
 
 client.run(TOKEN)
