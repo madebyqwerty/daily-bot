ebf63624028888bd66ab6f3d251861dcbf43830a
venca321 <login@vaclavparma.cz>
2023-04-24 13:13:02 +0200

    predelal timing system, ale jeste na to kouknu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from discord.ext import commands
+from discord.ext import commands, tasks
 import discord, datetime, asyncio, json, dotenv
 
 with open("settings.json", "r") as read_file: data = json.load(read_file)
@@ -15,19 +15,21 @@ client.remove_command('help')
 
 @client.event
 async def on_ready():
-    client.loop.create_task(timer())
+    my_task.start()
     print(f'{client.user} online!')
 
-async def timer():
-    while True:
-        await asyncio.sleep(60)
-        now = datetime.datetime.now()
-        current_time = now.strftime("%H:%M")
-        if current_time == REPORT_TIME and (not now.weekday() == 4 or not now.weekday() == 5):
-            channel = client.get_channel(CHANNEL_ID)
-            message = await channel.send(MESSAGE.replace("%date%", datetime.datetime.now().strftime("%d/%m")))
-            thread_channel = await channel.create_thread(name=datetime.datetime.now().strftime("%d/%m"), message=message)
-            await close_thread(thread_channel)
+@tasks.loop(hours=24)
+async def my_task():
+    channel = client.get_channel(CHANNEL_ID)
+    message = await channel.send(MESSAGE.replace("%date%", datetime.datetime.now().strftime("%d/%m")))
+    thread_channel = await channel.create_thread(name=datetime.datetime.now().strftime("%d/%m"), message=message)
+    await close_thread(thread_channel)
+
+@my_task.before_loop
+async def before_msg1():
+    now = datetime.datetime.now()
+    time = (abs(int(REPORT_TIME.split(":")[0])-now.hour)*60*60) + (abs(int(REPORT_TIME.split(":")[1])-now.minute)*60)
+    await asyncio.sleep(time)
 
 async def close_thread(thread_channel):
     await asyncio.sleep(60*(24-int(REPORT_TIME.split(":")[0])))
