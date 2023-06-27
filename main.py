fe893e7026cdc9b4c17c0b91cf0eae164e5a1423
Venca321 <login@vaclavparma.cz>
2023-06-27 21:28:52 +0200

    Add voice events

--- main.py
+++ main.py
@@ -21,8 +21,15 @@ async def on_ready():
 time = datetime.time(hour=REPORT_TIME.hour, minute=REPORT_TIME.minute, tzinfo=ZoneInfo("Europe/Prague"))
 @tasks.loop(time=time)
 async def my_task():
+    try:
+        voice_channel = client.get_channel(int(data["voice_channel_id"]))
+        event_time = datetime.datetime.now() + datetime.timedelta(days=1)
+        event_time = event_time.replace(hour=REPORT_TIME.hour, minute=REPORT_TIME.minute, second=0, microsecond=0)
+        invite = await voice_channel.create_invite(max_uses=1, max_age=60, reason=f'Daily {datetime.datetime.now().strftime("%d/%m")}')
+    except: invite = ""
+
     channel = client.get_channel(CHANNEL_ID)
-    message = await channel.send(MESSAGE.replace("%date%", datetime.datetime.now().strftime("%d/%m")))
+    message = await channel.send(MESSAGE.replace("%date%", datetime.datetime.now().strftime("%d/%m")).replace("%invite%", invite.url))
     await channel.create_thread(name=datetime.datetime.now().strftime("%d/%m"), message=message)
 
 client.run(TOKEN)
