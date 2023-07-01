45c362812d270c8fb6de666f426da5584b5166af
Venca321 <login@vaclavparma.cz>
2023-07-01 09:03:54 +0200

    Fix voice events, fix TOKEN

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 
 from discord.ext import commands, tasks
 from zoneinfo import ZoneInfo
-import discord, datetime, json, os
+import discord, datetime, json, os, dotenv
 
 with open("settings.json", "r") as read_file: data = json.load(read_file)
 REPORT_TIME = datetime.datetime.strptime(data["time"], '%H:%M')
 CHANNEL_ID = int(data["channel_id"])
 MESSAGE = data["message"]
-TOKEN = os.environ["TOKEN"]
+try: TOKEN = os.environ["TOKEN"]
+except: TOKEN = dotenv.dotenv_values(".env")["TOKEN"]
 
 intents = discord.Intents.all()
 client = commands.Bot(command_prefix='!', intents=intents)
@@ -23,9 +24,7 @@ time = datetime.time(hour=REPORT_TIME.hour, minute=REPORT_TIME.minute, tzinfo=Zo
 async def my_task():
     try:
         voice_channel = client.get_channel(int(data["voice_channel_id"]))
-        event_time = datetime.datetime.now() + datetime.timedelta(days=1)
-        event_time = event_time.replace(hour=REPORT_TIME.hour, minute=REPORT_TIME.minute, second=0, microsecond=0)
-        invite = await voice_channel.create_invite(max_uses=1, max_age=60, reason=f'Daily {datetime.datetime.now().strftime("%d/%m")}')
+        invite = await voice_channel.create_invite(max_uses=0, max_age=15*60, reason=f'Daily {datetime.datetime.now().strftime("%d/%m")}')
     except: invite = ""
 
     channel = client.get_channel(CHANNEL_ID)
