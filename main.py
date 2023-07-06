464ee929a49598c625b9877bab2f88a5487bc069
Venca321 <login@vaclavparma.cz>
2023-07-06 10:11:38 +0200

    Split daily and vote time

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from zoneinfo import ZoneInfo
 import discord, datetime, json, os, dotenv
 
 with open("settings.json", "r") as read_file: data = json.load(read_file)
-REPORT_TIME = datetime.datetime.strptime(data["time"], '%H:%M')
 CHANNEL_ID = int(data["channel_id"])
-MESSAGE = data["message"]
+REPORT_TIME0 = datetime.datetime.strptime(data["time0"], '%H:%M')
+MESSAGE0 = data["message0"]
+REPORT_TIME1 = datetime.datetime.strptime(data["time1"], '%H:%M')
+MESSAGE1 = data["message1"]
 try: TOKEN = os.environ["TOKEN"]
 except: TOKEN = dotenv.dotenv_values(".env")["TOKEN"]
 
@@ -16,22 +18,23 @@ client.remove_command('help')
 
 @client.event
 async def on_ready():
-    my_task.start()
+    task0.start()
+    task1.start()
     print(f'{client.user} online!')
 
-time = datetime.time(hour=REPORT_TIME.hour, minute=REPORT_TIME.minute, tzinfo=ZoneInfo("Europe/Prague"))
-@tasks.loop(time=time)
-async def my_task():
-    try:
-        voice_channel = client.get_channel(int(data["voice_channel_id"]))
-        invite = await voice_channel.create_invite(max_uses=0, max_age=15*60, reason=f'Daily {datetime.datetime.now().strftime("%d/%m")}')
-    except: invite = ""
-
+time0 = datetime.time(hour=REPORT_TIME0.hour, minute=REPORT_TIME0.minute, tzinfo=ZoneInfo("Europe/Prague"))
+@tasks.loop(time=time0)
+async def task0():
     channel = client.get_channel(CHANNEL_ID)
-    message = await channel.send(MESSAGE.replace("%date%", datetime.datetime.now().strftime("%d/%m")).replace("%invite%", invite.url))
-    thread = await channel.create_thread(name=datetime.datetime.now().strftime("%d/%m"), message=message)
-    thread_message = await thread.send("Bude zítra daily voice?")
-    await thread_message.add_reaction("❌")
-    await thread_message.add_reaction("✅")
+    message = await channel.send(MESSAGE0.replace("%date%", datetime.datetime.now().strftime("%d/%m")))
+    await channel.create_thread(name=datetime.datetime.now().strftime("%d/%m"), message=message)
+
+time1 = datetime.time(hour=REPORT_TIME1.hour, minute=REPORT_TIME1.minute, tzinfo=ZoneInfo("Europe/Prague"))
+@tasks.loop(time=time1)
+async def task1():
+    channel = client.get_channel(CHANNEL_ID)
+    message = await channel.send(MESSAGE1.replace("%date%", datetime.datetime.now().strftime("%d/%m")))
+    await message.add_reaction("❌")
+    await message.add_reaction("✅")
 
 client.run(TOKEN)
